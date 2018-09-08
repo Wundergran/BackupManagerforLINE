3fc473276437c1e51773f63ecceb3a329802d48b
frodstan <frode.standal94@gmail.com>
2018-09-08 17:02:24 +0200

    convert indents to tabs for .kt files

--- app/src/main/java/no/wunderapps/backupmanagerline/MainActivity.kt
+++ app/src/main/java/no/wunderapps/backupmanagerline/MainActivity.kt
@@ -6,14 +6,14 @@ import no.wunderapps.backupmanagerline.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow()
-        }
-    }
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.main_activity)
+		if (savedInstanceState == null) {
+			supportFragmentManager.beginTransaction()
+					.replace(R.id.container, MainFragment.newInstance())
+					.commitNow()
+		}
+	}
 
 }
--- app/src/main/java/no/wunderapps/backupmanagerline/ui/main/MainViewModel.kt
+++ app/src/main/java/no/wunderapps/backupmanagerline/ui/main/MainViewModel.kt
@@ -5,7 +5,7 @@ import androidx.lifecycle.ViewModel
 import no.wunderapps.backupmanagerline.data.ChatBackup
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+	// TODO: Implement the ViewModel
 	private var chatsBackedUp : MutableLiveData<MutableList<ChatBackup>>? = null
 
 	fun getChats() : MutableLiveData<MutableList<ChatBackup>>? {
